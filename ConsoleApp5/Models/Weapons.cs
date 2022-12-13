47d14028710058a51b6cc0bfc41f841addac616a
ca r215.02 <ca.r215.02@ca.local>
2022-12-13 13:41:22 +0400

    Weapon

---
+++ ConsoleApp5/Models/Weapons.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.Models
+{
+    internal class Weapons
+    {
+        int buller_capasity;
+        int buller_count;
+        double comp_discharge_second;
+        string weapon_mod = "tekli";
+        public int Buller_count 
+        { 
+            get
+            {
+                return buller_count;
+            }
+            set
+            {
+                if (value>0 && value<buller_capasity)
+                {
+                    buller_count = value;
+                }
+            }
+        }
+        public int Buller_Capasity
+        {
+            get
+            {
+                return buller_capasity;
+            }
+            set
+            {
+                if(value>5  && value<=40)
+                {
+                    buller_capasity = value;
+                }
+            }
+        }
+        public double Comp_discharge_second
+        {
+            get
+            {
+                return comp_discharge_second;
+            }
+            set
+            {
+                if (value > 6 && value < 60)
+                {
+                    comp_discharge_second = value;
+                }
+            }
+        }
+        public bool Weapon_Mod { get; set; }
+        
+        public Weapons(int buller_capasity, int buller_count, double comp_discharge_second)
+        {
+
+            Buller_Capasity = buller_capasity;
+            Buller_count = buller_count;
+            Comp_discharge_second = comp_discharge_second;
+            
+        }
+        public void Shoot(double bullercount) 
+        {
+            bullercount = this.buller_capasity;
+            if(bullercount>0)
+            {
+                Console.WriteLine("Gulle atildi");
+                bullercount = bullercount - 1;
+            }
+            else if (bullercount ==0)
+            {
+                Console.WriteLine("sizin gulleniz yoxdur");
+            }
+            
+        }
+        public void Show_info()
+        {
+            Console.WriteLine("gulle sayi :"+ buller_capasity);
+            Console.WriteLine("gulle sayi :" + comp_discharge_second);
+            Console.WriteLine("gulle sayi :" + weapon_mod);
+        }
+        public double Fire()
+        {
+            return comp_discharge_second / (buller_capasity - buller_count);
+        }
+        public void Reolad()
+        {
+            if(buller_count<buller_capasity)
+            {
+                buller_count = buller_count + (buller_capasity - buller_count);
+            }
+            Console.WriteLine(buller_count);
+        }
+        public int GetRemainBulletCount()
+        {
+            return buller_capasity - buller_count;
+        }
+        public void ChangeFireMode()
+        {
+            weapon_mod = "avto";
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,74 @@
+﻿using ConsoleApp5.Models;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        tryagain:
+            Console.WriteLine("Bullercountu daxil edin");
+            int buller_counnt = int.Parse(Console.ReadLine());
+            if (buller_counnt <0 || buller_counnt>40)
+            {
+                Console.WriteLine("dogru daxil edin");
+                goto tryagain;
+            }
+            tryagain1:
+            Console.WriteLine("Buller capastini daxil edin");
+            int buller_capasity = int.Parse(Console.ReadLine());
+            if(buller_capasity<5 || buller_capasity>40)
+            {
+                Console.WriteLine("dogru daxil edin");
+                goto tryagain1;
+            }
+              tryagain2:
+            Console.WriteLine("dargin bosalma saniyesini daxil edin");
+           
+            double comp_discharge_second = double.Parse(Console.ReadLine());
+            if(comp_discharge_second<6 || comp_discharge_second>50)
+            {
+                Console.WriteLine("dogru daxil edin");
+                goto tryagain2;
+            }
+        tryagain3:
+            //Console.WriteLine("atis modunu daxil edin");
+            //string weapon_mod = Console.ReadLine();
+            //if(weapon_mod.ToLower()!= "tek" && weapon_mod.ToLower()!="avto")
+            //{
+            //    Console.WriteLine("dogru daxil edin");
+            //    goto tryagain3;
+            //}
+
+            Weapons weapons = new Weapons(buller_capasity, buller_counnt, comp_discharge_second);
+            Console.WriteLine("secim edin");
+            string secim = Console.ReadLine();
+            switch (secim)
+            {
+                case "0":
+                    Console.WriteLine();
+                    break;
+                case "1":
+                    //weapons.Shoot();
+                    break;
+                case "2":
+                    weapons.Fire();
+                    break;
+                case "3":
+
+                    weapons.GetRemainBulletCount();
+                    break;
+                case "4":
+                    weapons.Reolad();
+                    break;
+                case "5":
+                    weapons.ChangeFireMode();
+                    break;
+                case "6":
+                    return;
+
+            }
+            
+        }
+    }
+}
